a0e55ff08c1d1fcf19b25af248a101ef37432eca
Mako Enari <mako9606@gmail.com>
2025-12-03 21:12:32 +0900

    ALLOWED_HOSTSに追加

--- travel-scheduler/frist_project/settings.py
+++ travel-scheduler/frist_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@#*l7@1b$(o6b&9j8y79n3km=-wtebyse^w+zj58*czol_xjv%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['makoe.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
