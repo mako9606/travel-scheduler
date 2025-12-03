feb3b69d4a5f73578f44db17dd51ebfc21514172
Mako Enari <mako9606@gmail.com>
2025-12-03 21:35:48 +0900

    STATICFILES_DIRSni

--- travel-scheduler/frist_project/settings.py
+++ travel-scheduler/frist_project/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'Portfolio' / 'static'
+    BASE_DIR / 'Portfolio' / 'portfolio_app' / 'static'
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
