738dda009481268dcfd11198b91a5a2808bff0ee
Mako Enari <mako9606@gmail.com>
2025-12-04 19:48:35 +0900

    staticフォルダ にportfolio_appフォルダ追加

--- travel-scheduler/frist_project/settings.py
+++ travel-scheduler/frist_project/settings.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'travel-scheduler' / 'Portfolio' / 'portfolio_app' / 'static',
+]
+
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
