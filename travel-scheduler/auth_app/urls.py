fd0281f847e8e0c54e8dbdace33831945de916c0
Mako Enari <mako9606@gmail.com>
2025-12-12 17:33:01 +0900

    loginのpath修正

--- travel-scheduler/auth_app/urls.py
+++ travel-scheduler/auth_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name='auth_app'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='auth_app/login.html'),
+         name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('home/', views.home_view, name='home'),
 ]
