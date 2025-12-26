7ce3bff03a2dd3cd750e564ff5bb195a223670e7
Mako Enari <mako9606@gmail.com>
2025-12-26 11:42:24 +0900

    PasswordResetViewのためpassword_reset_email追加、パスワード再設定フロー表示できるように整える

--- travel-scheduler/account/urls.py
+++ travel-scheduler/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from . import views
 
 
@@ -12,12 +13,14 @@ urlpatterns = [
    # ↓ログアウト処理
    path('logout/execute/', LogoutView.as_view(), name='logout_execute'),
    # ↓ログイン中のパスワード変更画面
-   path('password-complete/', views.password_complete, name=password_complete),
+   path('password-complete/', views.password_complete, name='password_complete'),
     
    # ↓パスワード再設定メール送信
    path('password_reset/',
          auth_views.PasswordResetView.as_view(
-            template_name="account/password_reset.html"
+            template_name="account/password_reset.html",
+            email_template_name='account/password_reset_email.html',
+            success_url=reverse_lazy('account:password_reset_done'),
          ),
          name='password_reset'),
     
@@ -29,9 +32,10 @@ urlpatterns = [
          name='password_reset_done'),
     
    #　↓パスワード再設定画面
-   path('password_reset_confirm/<uid64>/<token>',
+   path('password_reset_confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-            template_name="account/password_reset_confirm.html"
+            template_name="account/password_reset_confirm.html",
+            success_url=reverse_lazy('account:password_reset_complete'),
          ),
          name='password_reset_confirm'),
     
