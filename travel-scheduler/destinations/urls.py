7f62ea9712bd6d502da29e9a9f521aeaf1913fdc
Mako Enari <mako.jaz1006@gmail.com>
2026-01-10 17:18:49 +0900

    destination_search表示させる

--- travel-scheduler/destinations/urls.py
+++ travel-scheduler/destinations/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'destinations'
 
 urlpatterns = [
+    path('search/', views.destination_search, name='destination_search'),
 ]
--- travel-scheduler/destinations/views.py
+++ travel-scheduler/destinations/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def destination_search(request):
+    q = request.GET.get("q", "")
+
+    # 仮の検索結果（model実装に変更する）
+    destinations = []
+
+    return render(
+        request,
+        "destinations/destination_search.html",
+        {
+            "q": q,
+            "destinations": destinations,
+        }
+    )
