1d76d279cdf988f7b16ad7d009e7581888bbef0c
Mako Enari <mako.jaz1006@gmail.com>
2026-01-15 20:13:24 +0900

    account forms本実装のための記載追加

--- travel-scheduler/account/forms.py
+++ travel-scheduler/account/forms.py
@@ -11,3 +11,12 @@ class AccountEditForm(forms.ModelForm):
             "username" : "アカウント名",
             "email" : "メールアドレス",
         }
+     
+     
+     
+    # 「username/email をDBに保存する前に、前後の空白を削除してください」    
+    def clean_username(self):
+        return self.cleaned_data["username"].strip()
+    
+    def clean_email(self):
+        return self.cleaned_data["email"].strip()
