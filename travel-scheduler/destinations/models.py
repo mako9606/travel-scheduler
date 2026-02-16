f94f1af978cd67449e66c0bc0ef37d74e88d5395
Mako Enari <mako.jaz1006@gmail.com>
2026-02-16 16:48:40 +0900

    DB全削除（2回目）

--- travel-scheduler/destinations/models.py
+++ travel-scheduler/destinations/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Destination(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="destinations")
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    url = models.CharField(max_length=225, blank=True)
+    open_at = models.TimeField(null=True, blank=True)
+    close_at = models.TimeField(null=True, blank=True)
+    closed_day = models.CharField(max_length=20, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    memo = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.name
